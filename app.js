7caee4f80715ff2f5ffca41dea12c3ca97823fd6
Muhammad Ali Siddiqui <172058104+alideveloper15@users.noreply.github.com>
2024-12-05 18:39:20 +0500

    Alerts, Task Done!

---
+++ app.js
@@ -0,0 +1,6 @@
+alert("I am Muhammad Ali Siddiqui (alideveloper15).")
+alert("I am 14 years old, studying in 9th class.")
+alert("Error! Please enter a valid password.")
+alert("Welcome to Js Land...")
+alert("Happy Coding!")
+alert("Hello... I can run JS through my web browser's console")
