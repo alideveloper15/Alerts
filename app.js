eb73f86fc4ddabdded0e5331df1184f4ee06cb6e
Muhammad Ali Siddiqui <172058104+alideveloper15@users.noreply.github.com>
2025-06-20 15:13:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-alert("I am Muhammad Ali Siddiqui (alideveloper15).")
-alert("I am 14 years old, studying in 9th class.")
-alert("Error! Please enter a valid password.")
-alert("Welcome to Js Land...")
-alert("Happy Coding!")
-alert("Hello... I can run JS through my web browser's console")
+alert('Hello!')
+alert('Error! Please enter a valid password')
+alert(`Welcome to JS Land...
+Happy Coding!`)
+alert('Welcome to JS Land...')
+alert(`Happy Coding!
+Prevent thsi page from creating additonal dialogs`)
+console.log(alert(`Hello... I can run JS through my web browser's console`))
